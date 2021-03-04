7f72be5d3dfbee1b2c0ec0a3bf0dff5464b55a75
J-TKim <kkkd991020@naver.com>
2021-03-04 14:27:30 +0900

    2-04

--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+
 @bp.route('/hello')
 def hello_pybo():
-    return 'Hello, pybo!'
+    return 'Hello, Pybo!'
+
 
 @bp.route('/')
 def index():
